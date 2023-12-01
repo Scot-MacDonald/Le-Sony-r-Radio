e1e2e8a3369c53cfa3c33ef5396ccfbe2a52b8d9
Scot MacDonald <scotmacdonald@hotmail.com>
2023-12-01 12:46:11 +0100

    google auth

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,20 @@ import { SWRConfig } from "swr";
 import GlobalStyle from "../styles";
 import RootLayout from "@/components/Layout";
 import { SelectedTrackProvider } from "@/context/SelectedTrackContext";
+import { SessionProvider } from "next-auth/react";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, session }) {
   return (
     <>
       <SelectedTrackProvider>
         <GlobalStyle />
         <RootLayout>
           <SWRConfig value={{ fetcher }}>
-            <Component {...pageProps} />
+            <SessionProvider session={session}>
+              <Component {...pageProps} />
+            </SessionProvider>
           </SWRConfig>
         </RootLayout>
       </SelectedTrackProvider>
