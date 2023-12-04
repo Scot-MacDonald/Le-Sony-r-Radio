f972814610c77b3ad06335446acf48a62f4b902a
Scot MacDonald <scotmacdonald@hotmail.com>
2023-12-04 10:25:54 +0100

    start search

---
+++ pages/search/SearchInput.js
@@ -0,0 +1,13 @@
+"use client";
+
+export default function SearchInput() {
+  return (
+    <>
+      <div>SearchInput</div>
+      <div>
+        <input type="text" name="search" id="search" />
+      </div>
+      ;
+    </>
+  );
+}
---
+++ pages/search/index.js
@@ -0,0 +1,10 @@
+import SearchInput from "./searchInput";
+
+export default function Search() {
+  return (
+    <div>
+      <h1>Search</h1>
+      <SearchInput />
+    </div>
+  );
+}
