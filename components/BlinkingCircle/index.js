8a7af92e2dbba2286d5efd5c9b1798ccd135bb6a
Scot MacDonald <scotmacdonald@hotmail.com>
2023-12-11 17:28:56 +0100

    blinking circle

---
+++ components/BlinkingCircle/index.js
@@ -0,0 +1,12 @@
+// BlinkingCircle.js
+import React, { useState, useEffect } from "react";
+
+const BlinkingCircle = () => {
+  return (
+    <svg height="20" width="20">
+      <circle cx="10" cy="10" r="5" fill="blue" />
+    </svg>
+  );
+};
+
+export default BlinkingCircle;
