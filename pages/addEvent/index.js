60242940e06594dc30a8aa4a53779d9f872361a4
Scot MacDonald <scotmacdonald@hotmail.com>
2024-02-20 12:24:19 +0100

    modification ensures that useRouter() is only used on the client-side

--- pages/addEvent/index.js
+++ pages/addEvent/index.js
@@ -1,6 +1,50 @@
+// import useSWR from "swr";
+// import { useSession } from "next-auth/react";
+// import { useRouter } from "next/router";
+// import EventForm from "@/components/EventForm";
+// import styles from "@/styles/addMix.module.css";
+
+// export default function AddEventPage() {
+//   const { data: session } = useSession();
+//   const { mutate } = useSWR("/api/events");
+//   const router = useRouter();
+
+//   async function handleSubmit(formData) {
+//     if (!session) {
+//       // User is not logged in, redirect to the home page
+//       router.push("/");
+//       return;
+//     }
+
+//     const response = await fetch("/api/events", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(formData),
+//     });
+
+//     if (response.ok) {
+//       mutate();
+//     }
+//   }
+
+//   if (!session) {
+//     router.push("/");
+//     return null;
+//   }
+
+//   return (
+//     <>
+//       <EventForm onSubmit={handleSubmit} value="" />
+//     </>
+//   );
+// }
+
+import { useEffect } from "react";
 import useSWR from "swr";
 import { useSession } from "next-auth/react";
-import useRouter from "next/router";
+import { useRouter } from "next/router";
 import EventForm from "@/components/EventForm";
 import styles from "@/styles/addMix.module.css";
 
@@ -8,6 +52,13 @@ export default function AddEventPage() {
   const { data: session } = useSession();
   const { mutate } = useSWR("/api/events");
   const router = useRouter();
+
+  useEffect(() => {
+    // Redirect if session is not available
+    if (!session) {
+      router.push("/");
+    }
+  }, [session, router]);
 
   async function handleSubmit(formData) {
     if (!session) {
@@ -30,7 +81,7 @@ export default function AddEventPage() {
   }
 
   if (!session) {
-    router.push("/");
+    // If session is not available, redirect should be handled in useEffect
     return null;
   }
 
--- pages/addMix/index.js
+++ pages/addMix/index.js
@@ -1,3 +1,49 @@
+// import useSWR from "swr";
+// import { useSession } from "next-auth/react";
+// import { useRouter } from "next/router";
+// import MixForm from "@/components/MixForm";
+// import styles from "@/styles/addMix.module.css";
+
+// export default function AddMixPage() {
+//   const { data: session } = useSession();
+//   const { mutate } = useSWR("/api/mixes");
+//   const router = useRouter();
+
+//   async function handleSubmit(formData) {
+//     if (!session) {
+//       // User is not logged in, redirect to the home page
+//       router.push("/");
+//       return;
+//     }
+
+//     const response = await fetch("/api/mixes", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(formData),
+//     });
+
+//     if (response.ok) {
+//       mutate();
+//     }
+//   }
+
+//   // If user is not logged in, redirect to the home page
+//   if (!session) {
+//     // You can also display a message or additional content if needed
+//     router.push("/");
+//     return null; // This can be any content you want to display when redirecting
+//   }
+
+//   return (
+//     <>
+//       <MixForm onSubmit={handleSubmit} value="" />
+//     </>
+//   );
+// }
+
+import { useEffect } from "react";
 import useSWR from "swr";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
@@ -8,6 +54,13 @@ export default function AddMixPage() {
   const { data: session } = useSession();
   const { mutate } = useSWR("/api/mixes");
   const router = useRouter();
+
+  useEffect(() => {
+    // Redirect if session is not available
+    if (!session) {
+      router.push("/");
+    }
+  }, [session, router]);
 
   async function handleSubmit(formData) {
     if (!session) {
@@ -29,11 +82,9 @@ export default function AddMixPage() {
     }
   }
 
-  // If user is not logged in, redirect to the home page
   if (!session) {
-    // You can also display a message or additional content if needed
-    router.push("/");
-    return null; // This can be any content you want to display when redirecting
+    // If session is not available, redirect should be handled in useEffect
+    return null;
   }
 
   return (
