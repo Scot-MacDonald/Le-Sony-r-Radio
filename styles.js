dbb342e718d1fe2ebd9bc5720ee6890898d32127
Scot MacDonald <scotmacdonald@hotmail.com>
2024-02-22 16:18:40 +0100

    mui

--- styles.js
+++ styles.js
@@ -1,8 +1,123 @@
+// import { createGlobalStyle } from "styled-components";
+
+// export default createGlobalStyle`
+
+//   *,
+//   *::before,
+//   *::after {
+//     box-sizing: border-box;
+//   }
+
+//   :root {
+//     --bg-color: black;
+//     --font-color: white;
+//     --colorLive: black;
+//     --fontReverse: black;
+//     --secondary:#ccc;
+
+//   }
+
+//   [data-theme='dark'],
+//   [data-theme='dark'] body {
+//     color: #242329;
+//     --bg-color: white;
+//     --font-color: black;
+//     --colorLive: white;
+//     --fontReverse: white;
+//     --secondary:black;
+
+//   }
+
+//   body {
+//    background:var(--bg-color);
+//     color:var( --font-color);
+//     font-family: Arial, Helvetica, sans-serif;
+//     font-size: 20px;
+//   }
+
+//   p{
+//     font-size:16px;
+//   }
+
+//   a {
+//     color: var( --font-color);
+//     text-decoration: none;
+//   }
+
+//   * {
+//     box-sizing: border-box;
+//     padding: 0;
+//     margin: 0;
+//   }
+
+//   ul {
+//     list-style-type: none;
+//   }
+//   main{
+//     margin-top:82px;
+
+//   }
+//  h1{
+//   font-size: 28px;
+//   line-height: 32px;
+//   margin:10px;
+
+// }
+// h2{
+//   font-size: 28px;
+//   line-height: 32px;
+//   margin: 10px;
+// }
+
+//   footer {
+//     bottom: 0;
+//     position: fixed;
+//     width: 100%;
+//   }
+
+//   .g-background-default {
+//     background-color: black;
+//   }
+
+//   .css-1nvnyqx-MuiPaper-root-MuiDrawer-paper{
+//     background-color:black!important;
+//     margin-top:81px;
+//     z-index:1400!important;
+//   }
+
+//   .css-68zbsl {
+//     width: auto;
+//     border: 1px solid white;}
+
+//     .css-1e6y48t-MuiButtonBase-root-MuiButton-root{
+//       border-left:white solid 1px!important;
+//       border-right:white solid 1px!important;
+//       border-bottom:white solid 1px!important;
+//       border-radius:0!important;
+//       padding:0!important;
+//       min-width:26px!important;
+//       max-width:30px!important;
+//       max-height:30px!important;
+//     }
+//     .css-1e6y48t-MuiButtonBase-root-MuiButton-root:hover{
+//       background-color:black;
+//     }
+
+//     .dark{
+//       min-width:26px;
+//       max-width:30px;
+//       max-height:30px;
+//       border-right:white solid 1px;
+//       border-bottom:white solid 1px;
+//       background-color:transparent;
+//     }
+
+// `;
+
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
 
-    
   *,
   *::before,
   *::after {
@@ -15,9 +130,8 @@ export default createGlobalStyle`
     --colorLive: black;
     --fontReverse: black;
     --secondary:#ccc;
-   
-  }
 
+  }
 
   [data-theme='dark'],
   [data-theme='dark'] body {
@@ -27,7 +141,7 @@ export default createGlobalStyle`
     --colorLive: white;
     --fontReverse: white;
     --secondary:black;
-  
+
   }
 
   body {
@@ -36,36 +150,34 @@ export default createGlobalStyle`
     font-family: Arial, Helvetica, sans-serif;
     font-size: 20px;
   }
- 
-
 
   p{
     font-size:16px;
   }
-  
+
   a {
     color: var( --font-color);
     text-decoration: none;
   }
-  
+
   * {
     box-sizing: border-box;
     padding: 0;
     margin: 0;
   }
-  
+
   ul {
     list-style-type: none;
   }
   main{
     margin-top:82px;
-    
+
   }
  h1{
   font-size: 28px;
   line-height: 32px;
   margin:10px;
-  
+
 }
 h2{
   font-size: 28px;
@@ -79,9 +191,9 @@ h2{
     width: 100%;
   }
 
-  // .g-background-default {
-  //   background-color: black;
-  // }
+  .g-background-default {
+    background-color: black;
+  }
 
   .css-oqbj7d{
     background-color:black!important;
@@ -89,9 +201,9 @@ h2{
     z-index:1400!important;
   }
 
-  // .css-68zbsl {
-  //   width: auto;
-  //   border: 1px solid white;}
+  .css-68zbsl {
+    width: auto;
+    border: 1px solid white!important;}
 
     .css-1ujsas3{
       border-left:white solid 1px!important;
@@ -103,10 +215,10 @@ h2{
       max-width:30px!important;
       max-height:30px!important;
     }
-  //   .css-1e6y48t-MuiButtonBase-root-MuiButton-root:hover{
-  //     background-color:black;
-  //   }
-  
+    .css-1e6y48t-MuiButtonBase-root-MuiButton-root:hover{
+      background-color:black;
+    }
+
     .dark{
       min-width:26px;
       max-width:30px;
@@ -116,6 +228,4 @@ h2{
       background-color:transparent;
     }
 
-    
-    
 `;
